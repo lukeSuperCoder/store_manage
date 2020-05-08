91e0792f65c016fc5184d5eea83946a5e6a982c2
一个有理想的苦逼程序员 <1342018842@qq.com>
2020-05-08 17:38:33 +0800

    完成用户列表功能开发

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -14,7 +14,7 @@
                     </el-input>
                 </el-col>
                 <el-col :span="4">
-                    <el-button type="primary" class="user_add_button" @click="user_dialogVisible=true">添加用户</el-button>
+                    <el-button type="primary" class="user_add_button" @click="user_add_dialogVisible=true">添加用户</el-button>
                 </el-col>
             </el-row>
                 <template>
@@ -54,9 +54,8 @@
                                 prop="user_set"
                                 label="操作">
                             <template slot-scope="scope">
-                                {{scope.row.assign}}
-                                <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-                                <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+                                <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditMessage(scope.row.id)"></el-button>
+                                <el-button type="danger" icon="el-icon-delete" size="mini" @click="showDeleteMessage(scope.row)"></el-button>
                                 <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
                                     <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
                                 </el-tooltip>
@@ -74,34 +73,62 @@
                     </el-pagination>
                 </template>
             <el-dialog
-                            title="提示"
-                            :visible.sync="user_dialogVisible"
-                            width="50%"
-                            :before-close="handleClose">
-                    <el-form :model="user_addForm" status-icon :rules="user_addFormRules" ref="user_addFormRef" label-width="100px"
-                             class="demo-ruleForm">
-                        <el-form-item label="用户名" prop="username">
-                            <el-input  v-model="user_addForm.username" autocomplete="off"></el-input>
-                        </el-form-item>
-                        <el-form-item label="密码" prop="password">
-                            <el-input type="password" v-model="user_addForm.password" autocomplete="off"></el-input>
-                        </el-form-item>
-                        <el-form-item label="邮箱" prop="email">
-                            <el-input v-model="user_addForm.email"></el-input>
-                        </el-form-item>
-                        <el-form-item label="手机号" prop="phone">
-                            <el-input v-model="user_addForm.phone"></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                            <el-button @click="resetForm('ruleForm')">重置</el-button>
-                        </el-form-item>
-                    </el-form>
-                    <span slot="footer" class="dialog-footer">
-                    <el-button @click="user_dialogVisible = false">取 消</el-button>
-                    <el-button type="primary" @click="user_dialogVisible = false">确 定</el-button>
+                    title="提示"
+                    :visible.sync="user_add_dialogVisible"
+                    width="50%" @close="closeAddDialog">
+                <el-form :model="user_addForm" status-icon :rules="user_addFormRules" ref="user_addFormRef"
+                         label-width="100px"
+                         class="demo-ruleForm">
+                    <el-form-item label="用户名" prop="username">
+                        <el-input v-model="user_addForm.username" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input type="password" v-model="user_addForm.password" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="user_addForm.email"></el-input>
+                    </el-form-item>
+                    <el-form-item label="手机号" prop="mobile">
+                        <el-input v-model="user_addForm.mobile"></el-input>
+                    </el-form-item>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                    <el-button @click="user_add_dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="submitAddForm">确 定</el-button>
           </span>
-        </el-dialog>
+            </el-dialog>
+            <el-dialog
+                    title="提示"
+                    :visible.sync="user_update_dialogVisible"
+                    width="50%" @close="closeUpdateDialog" :close-on-click-modal="false">
+                <el-form :model="user_updateForm" status-icon :rules="user_updateFormRules" ref="user_updateFormRef"
+                         label-width="100px"
+                         class="demo-ruleForm">
+                    <el-form-item label="用户名" prop="username">
+                        <el-input v-model="user_updateForm.username" autocomplete="off" :disabled="true"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="user_updateForm.email"></el-input>
+                    </el-form-item>
+                    <el-form-item label="手机号" prop="mobile">
+                        <el-input v-model="user_updateForm.mobile"></el-input>
+                    </el-form-item>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                    <el-button @click="user_update_dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="submitUpdateForm">确 定</el-button>
+          </span>
+            </el-dialog>
+            <el-dialog
+                    title="提示"
+                    :visible.sync="user_delete_dialogVisible"
+                    width="30%">
+                    <span>你确定要删除用户{{this.user_updateForm.username}}吗？</span>
+                    <span slot="footer" class="dialog-footer">
+                    <el-button @click="user_delete_dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="deleteUser">确 定</el-button>
+                  </span>
+            </el-dialog>
         </div>
         </el-card>
     </div>
@@ -113,20 +140,36 @@ export default {
         this.getUserList();
     },
     data(){
+        //验证手机号规则
+        const checkMobile = (rule, value, cb) => {
+                let reg = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+                if (reg.test(value)) {
+                    cb()
+                } else {
+                    cb(new Error('手机号码格式不正确'))
+                }
+            }
         return{
             user_tableDate: [],
             user_queryInfo:{
                 query: '',
                 pagenum: 1,
-                pagesize: 1
+                pagesize: 5
             },
             user_pageTotal:0,
-            user_dialogVisible:false,
+            user_add_dialogVisible:false,   //添加用户对话框
+            user_update_dialogVisible:false,//修改用户信息对话框
+            user_delete_dialogVisible:false,//删除用户对话框
             user_addForm:{
                 username:'',
                 password:'',
                 email:'',
-                phone:''
+                mobile:''
+            },
+            user_updateForm:{
+                username:'',
+                email:'',
+                mobile:''
             },
             user_addFormRules:{
                 username:[
@@ -147,20 +190,38 @@ export default {
                 ],
                 email:[
                     {
-                        required:true,message:"请输入邮箱",trigger:"blur"
+                        required:true,message:"请输入用户名",trigger:"blur"
                     },
                     {
                         type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']
                     }
                 ],
-                phone:[
+                mobile: [
                     {
-                        required:true,message:"请输入手机号",trigger:"blur"
+                        required:true,message:"请输入用户名",trigger:"blur"
                     },
                     {
-                        type: 'number', message: '手机号必须为数字值'
+                        validator: checkMobile, trigger: 'blur'
+                    }
+                ]
+            },
+            user_updateFormRules:{
+                email:[
+                    {
+                        required:true,message:"请输入用户名",trigger:"blur"
+                    },
+                    {
+                        type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']
                     }
                 ],
+                mobile: [
+                    {
+                        required:true,message:"请输入用户名",trigger:"blur"
+                    },
+                    {
+                        validator: checkMobile, trigger: 'blur'
+                    }
+                ]
             }
         }
     },
@@ -192,6 +253,75 @@ export default {
             //console.log(`当前页: ${val}`);
             this.user_queryInfo.pagenum=val;
             this.getUserList();
+        },
+        //添加用户
+        submitAddForm() {
+            this.$refs['user_addFormRef'].validate(async valid => {
+                if (valid) {
+                    const {data: result} = await this.$http.post("users", this.user_addForm);
+                    if (result.meta.status == 201) {
+                        this.$message.success(result.meta.msg);
+                        this.closeAddDialog();
+                    } else {
+                        this.$message.error(result.meta.msg);
+                    }
+                } else {
+                    return false;
+                }
+            });
+        },
+        //重置添加用户表单
+        closeAddDialog(){
+            this.user_add_dialogVisible = false;
+            this.$refs['user_addFormRef'].resetFields();
+        },
+        //为修改栏提供默认用户信息
+        async showEditMessage(id){
+           const {data:result} = await this.$http.get(`users/${id}`);
+           if(result.meta.status==200){
+               this.$message.success(result.meta.msg);
+               this.user_updateForm=result.data;
+           }else {
+               this.$message.error(result.meta.msg);
+           }
+            this.user_update_dialogVisible=true;
+        },
+        //提交更新用户信息
+        submitUpdateForm(){
+            this.$refs['user_updateFormRef'].validate(async valid => {
+                if (valid) {
+                    const {data: result} = await this.$http.put(`users/${this.user_updateForm.id}`, this.user_updateForm);
+                    if (result.meta.status == 200) {
+                        this.$message.success(result.meta.msg);
+                        this.closeUpdateDialog();
+                        this.getUserList();
+                    } else {
+                        this.$message.error(result.meta.msg);
+                    }
+                } else {
+                    return false;
+                }
+            });
+        },
+        //重置修改用户表单
+        closeUpdateDialog(){
+            this.user_update_dialogVisible = false;
+            this.$refs['user_updateFormRef'].resetFields();
+        },
+        //删除用户信息
+        showDeleteMessage(userInfo){
+            this.user_delete_dialogVisible=true;
+            this.user_updateForm=userInfo;
+        },
+        async deleteUser(){
+            const {data: result} = await this.$http.delete(`users/${this.user_updateForm.id}`);
+            if (result.meta.status == 200) {
+                this.$message.success(result.meta.msg);
+                this.user_delete_dialogVisible=false;
+                this.getUserList();
+            } else {
+                this.$message.error(result.meta.msg);
+            }
         }
     }
 }
